a8f1fcbd9822c68b58c1dba47b619996582a416b
Ankan Debnath <ankan_debnath@epam.com>
2025-03-20 01:06:03 +0530

    added duplicate media check testcase in add-media command

--- tests/2_add_media_test.py
+++ tests/2_add_media_test.py
@@ -31,9 +31,16 @@ def test_adding_media(review_system, user_name, media_type, media_name):
     result = review_system.add_media(MediaFactory.create_media(user_name, media_type, media_name))
     assert result == f"[green]Media added \nType : {media_type}, \nName : {media_name}[/green]"
 
-# def test_duplicate_media(review_system):
-#     user_name = "test_user"
-#     media_type = "movie"
-#     media_name = "Demo"
-#     result = review_system.add_media(MediaFactory.create_media(user_name, media_type, media_name))
-#     assert result == "[red]Error:[/red] Media Type must be movie, song or web_show"
+@pytest.mark.parametrize(
+    "user_name, media_type, media_name",
+    [
+        ("test_user", "movie", "demo1"),
+        ("test_user", "song", "demo2"),
+        ("test_user", "web_show", "demo3"),
+    ],
+    ids=["movie", "song", "wbe_show"]
+
+)
+def test_duplicate_media(review_system, user_name, media_type, media_name):
+    result = review_system.add_media(MediaFactory.create_media(user_name, media_type, media_name))
+    assert result == f"[red]Error:[/red] Add Media error, message : Media already exists"
